073180b51ddb3421068090d99764497c136c0f46
csjh <61282104+csjh@users.noreply.github.com>
2023-06-14 17:26:36 -0400

    initial commit

---
+++ components/index.js
@@ -0,0 +1,3 @@
+// Reexport your entry components here
+
+export { default as Hello } from './Hello.svelte';
---
+++ pages/index.md
@@ -0,0 +1,5 @@
+<script>
+    import { Hello } from "$lib";
+</script>
+
+<Hello name="world" />
